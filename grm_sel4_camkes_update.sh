6374896e92dc01e3d9cf94f64e2ebac32c26d347
Axel Heider <axel.heider@hensoldt-cyber.com>
2020-05-26 21:10:58 +0200

    [DEV] make script more generic

--- grm_sel4_camkes_update.sh
+++ grm_sel4_camkes_update.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -ue
 
-
 # get the directory the script is located in
 SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 
-cd ${SCRIPT_DIR}/seos_tests/src/seos_sandbox/sdk-sel4-camkes
 
-
-function checkout_cutting_edge()
+#-------------------------------------------------------------------------------
+function update_cutting_edge()
 {
     (
         SUBDIR=$1
@@ -20,24 +18,41 @@ function checkout_cutting_edge()
         cd ${SUBDIR}
         git remote -v
         git fetch --all
-        git checkout -B ${BRANCH} github/${BRANCH}
-        git pull
-        git push origin ${BRANCH}
+        (
+            set -x
+            git checkout -B ${BRANCH} github/${BRANCH}
+            git pull
+            git push origin ${BRANCH}
+        )
     )
 }
 
 
-checkout_cutting_edge capdl
+#-------------------------------------------------------------------------------
+function sel4_camkes_update_cutting_edge()
+{
+    cd seos_sandbox/sdk-sel4-camkes
+    (
+        update_cutting_edge capdl
 
-checkout_cutting_edge kernel
+        update_cutting_edge kernel
 
-checkout_cutting_edge libs/musllibc sel4
-checkout_cutting_edge libs/sel4_libs
-checkout_cutting_edge libs/sel4_project_libs
-checkout_cutting_edge libs/sel4_util_libs
-checkout_cutting_edge libs/sel4runtime
+        update_cutting_edge libs/musllibc sel4
+        update_cutting_edge libs/sel4_libs
+        update_cutting_edge libs/sel4_project_libs
+        update_cutting_edge libs/sel4_util_libs
+        update_cutting_edge libs/sel4runtime
 
-checkout_cutting_edge tools/camkes
-checkout_cutting_edge tools/seL4
+        update_cutting_edge tools/camkes
+        update_cutting_edge tools/seL4
 
-# checkout_cutting_edge tools/riscv-pk
+        # update_cutting_edge tools/riscv-pk
+    )
+}
+
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+# we expect to be in seos_tests
+sel4_camkes_update_cutting_edge
