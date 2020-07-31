3e72890e43b599257e75a0a90bea8bf0ee3afeb2
Thomas Böhm <thomas.boehm@hensoldt-cyber.com>
2020-07-31 17:23:40 +0200

    [DEV] Remove tag referencing the docker registry

--- docker_to_bz2.sh
+++ docker_to_bz2.sh
@@ -18,11 +18,17 @@ function pull_and_archive_image()
     # "trentos_build:trentos_0.9" -> "trentos_build-0.9"
     IMAGE_ARCHIVE=${IMAGE_SHORT_ID%:*}_${IMAGE_SHORT_ID##*_}
 
+    # create new tag without registry name
+    docker tag ${IMAGE_ID} ${IMAGE_SHORT_ID}
+
+    # remove the tag containing the registry name
+    docker rmi ${IMAGE_ID}
+
     echo "saving image to ${IMAGE_ARCHIVE} ..."
 
-    # docker save ${IMAGE_ID} | pv | gzip > ${IMAGE_ARCHIVE}.gz
-    docker save ${IMAGE_ID} | pv | bzip2 > ${IMAGE_ARCHIVE}.bz2
-    # docker save ${IMAGE_ID} | pv | pxz > ${IMAGE_ARCHIVE}.xz
+    # docker save ${IMAGE_SHORT_ID} | pv | gzip > ${IMAGE_ARCHIVE}.gz
+    docker save ${IMAGE_SHORT_ID} | pv | bzip2 > ${IMAGE_ARCHIVE}.bz2
+    # docker save ${IMAGE_SHORT_ID} | pv | pxz > ${IMAGE_ARCHIVE}.xz
 
     sha256sum ${IMAGE_ARCHIVE}.bz2
 
