44cc4915c3410526d9d3f11c683dd6b8cc732166
Benedikt Driessen <benedikt.driessen@hensoldt-cyber.de>
2020-11-19 16:00:43 +0100

    [NEW] Add script to split off component folders from sandbox into repos

---
+++ component_to_git.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#
+# Usage: 
+#   ./component_to_git.sh <component-name> <repository-name>
+#
+# Will push all commits related to the component folder into the git repository 
+# in the branch "old-commits". 
+#
+# Example: 
+#   ./component_to_git.sh RamDisk ramdisk
+#
+ 
+COMP=$1
+GIT=$2
+
+# Branch name
+BRANCH=old-commits
+
+git clone -b integration ssh://git@bitbucket.hensoldt-cyber.systems:7999/seos/sandbox.git $COMP
+cd $COMP
+git checkout -b $BRANCH `git subtree split --prefix components/$COMP/`
+git remote add neworigin ssh://git@bitbucket.hensoldt-cyber.systems:7999/sc/$GIT.git
+git push -u neworigin $BRANCH
+cd ..
+rm -rf $COMP
