4e93530fdf9a7f620b3ea2b6eb366193238a949b
Axel Heider <axel.heider@hensoldt-cyber.de>
2020-12-07 03:23:30 +0100

    [DEV] version for SD cards and SD card switcher

--- cp_img.sh
+++ cp_img.sh
@@ -1,16 +1,32 @@
 #!/bin/bash -uex
 
 # get the directory the script is located in
-#SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 
-TEST_SYSTEM=build-rpi3-Debug-test_filesystem
-IMAGE=seos_tests/${TEST_SYSTEM}/images/os_image.bin
+SEOS_TEST=${SCRIPT_DIR}/../seos_tests
 
-REMOTE_HOST=192.168.82.31
-REMOTE_USER=root
-REMOTE_DIR=/root/axehei01
+SD_MUX_CTRL=${SEOS_TEST}/src/ta/common/board_automation/bin/sd-mux-ctrl
+SD_MUX_SN=202005170006
 
-scp ${IMAGE} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
+SD_MOUNT=/media/axehei01/00F0-D515
 
-ssh -t ${REMOTE_USER}@${REMOTE_HOST} "cd ${REMOTE_DIR}; ./ctrl-raspi.sh update"
+#TEST_SYSTEM=build-rpi3-Debug-test_filesystem
+TEST_SYSTEM=build-nitrogen6sx-Debug-test_network_api
 
+# IMAGE=seos_tests/${TEST_SYSTEM}/images/os_image.bin
+IMAGE=${SEOS_TEST}/${TEST_SYSTEM}/images/os_image.bin
+
+#REMOTE_HOST=192.168.82.31
+#REMOTE_USER=root
+#REMOTE_DIR=/root/axehei01
+#scp ${IMAGE} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
+#ssh -t ${REMOTE_USER}@${REMOTE_HOST} "cd ${REMOTE_DIR}; ./ctrl-raspi.sh update"
+
+
+${SD_MUX_CTRL} -e ${SD_MUX_SN} -s
+sleep 2
+cp ${IMAGE} ${SD_MOUNT}/
+ls -l ${SD_MOUNT}
+sync
+umount ${SD_MOUNT}
+${SD_MUX_CTRL} -e ${SD_MUX_SN} -d
