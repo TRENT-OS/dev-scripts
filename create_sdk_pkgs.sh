daffe4a8c9d80f2484def58e422d8f9855769298
Axel Heider <axel.heider@hensoldt-cyber.de>
2020-12-11 20:44:29 +0100

    [DEV] update SDK package creation script

--- create_sdk_pkgs.sh
+++ create_sdk_pkgs.sh
@@ -1,17 +1,25 @@
 #!/bin/bash -uex
 
+RELEASE_VERSION="1.1"
+RELEASE_DATE="UTC 2020-12-11 18:00:00"
+
+INPUT_ID=18
+
+#-------------------------------------------------------------------------------
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
-INPUT_PACKAGE=sdk-package-34.bz2
-
-RELEASE_DATE="UTC 2020-08-03 18:00:00"
-RELEASE_VERSION="1.0"
+INPUT_PACKAGE="sdk-package-v${RELEASE_VERSION}-${INPUT_ID}.bz2"
 
 ARCHIVE_SMALL=TRENTOS-M_SDK_no_docker_${RELEASE_VERSION}
 ARCHIVE_BIG=TRENTOS-M_SDK_${RELEASE_VERSION}
 
 WORKSPACE_CREATE=${TIMESTAMP}/pkg
 WORKSPACE_VERIFY=${TIMESTAMP}/pkg_vfy
+
+DOCKER_IAMGES=(
+    trentos_build_${RELEASE_VERSION}.bz2 \
+    trentos_test_${RELEASE_VERSION}.bz2 \
+)
 
 #-------------------------------------------------------------------------------
 function do_tar()
@@ -41,39 +49,25 @@ function do_untar()
 #-------------------------------------------------------------------------------
 do_untar ${INPUT_PACKAGE} ${WORKSPACE_CREATE}/sdk
 
-ln -s sdk/doc/pdf/TRENTOS-M_GettingStarted_SDK_V1.0.pdf ${WORKSPACE_CREATE}/GettingStarted.pdf
+ln -s sdk/doc/pdf/TRENTOS-M_GettingStarted_SDK_V${RELEASE_VERSION}.pdf ${WORKSPACE_CREATE}/GettingStarted.pdf
 ls -l ${WORKSPACE_CREATE}
 
 do_tar ${TIMESTAMP}/${ARCHIVE_SMALL}.bz2 ${WORKSPACE_CREATE}
-
 do_untar ${TIMESTAMP}/${ARCHIVE_SMALL}.bz2 ${WORKSPACE_VERIFY}
-
 diff -r ${WORKSPACE_CREATE} ${WORKSPACE_VERIFY}
-
 
 rm -rf ${WORKSPACE_VERIFY}
 
-
-mkdir ${WORKSPACE_CREATE}/docker
-
-sha256sum \
-    trentos_build_1.0.bz2 \
-    trentos_test_1.0.bz2 \
-    >${WORKSPACE_CREATE}/docker/trentos_1.0.sha256sums
-
-cp \
-    trentos_build_1.0.bz2 \
-    trentos_test_1.0.bz2 \
-    ${WORKSPACE_CREATE}/docker/
-
+OUT_BASE_DOCKER=${WORKSPACE_CREATE}/docker
+mkdir -p ${OUT_BASE_DOCKER}
+sha256sum ${DOCKER_IAMGES[@]} >${OUT_BASE_DOCKER}/trentos_${RELEASE_VERSION}.sha256sums
+cp ${DOCKER_IAMGES[@]} ${OUT_BASE_DOCKER}
 
 do_tar ${TIMESTAMP}/${ARCHIVE_BIG}.bz2 ${WORKSPACE_CREATE}
-
 do_untar ${TIMESTAMP}/${ARCHIVE_BIG}.bz2 ${WORKSPACE_VERIFY}
-
 diff -r ${WORKSPACE_CREATE} ${WORKSPACE_VERIFY}
 
 (
     cd ${TIMESTAMP}
-    sha256sum ${ARCHIVE_SMALL}.bz2 ${ARCHIVE_BIG}.bz2 > trentos_SDK_v1.0.sha256sums
+    sha256sum ${ARCHIVE_SMALL}.bz2 ${ARCHIVE_BIG}.bz2 > trentos_SDK_v${RELEASE_VERSION}.sha256sums
 )
