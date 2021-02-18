fe90d29a21a1868c5b32c04ed1e55f64eef25b54
Axel Heider <axel.heider@hensoldt-cyber.de>
2021-02-18 16:55:39 +0100

    [DEV] update script for 1.2 release needs

--- create_sdk_pkgs.sh
+++ create_sdk_pkgs.sh
@@ -1,31 +1,61 @@
 #!/bin/bash -uex
 
-RELEASE_VERSION="1.1"
-RELEASE_DATE="UTC 2020-12-11 18:00:00"
+RELEASE_VERSION="1.2"
+RELEASE_DATE="UTC 2021-02-18 18:00:00"
 
-INPUT_ID=18
+INPUT_ID=26
 
 #-------------------------------------------------------------------------------
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
-INPUT_PACKAGE="sdk-package-v${RELEASE_VERSION}-${INPUT_ID}.bz2"
-
-ARCHIVE_SMALL=TRENTOS-M_SDK_no_docker_${RELEASE_VERSION}
-ARCHIVE_BIG=TRENTOS-M_SDK_${RELEASE_VERSION}
-
-WORKSPACE_CREATE=${TIMESTAMP}/pkg
-WORKSPACE_VERIFY=${TIMESTAMP}/pkg_vfy
+INPUT_PACKAGE="sdk-package-v${RELEASE_VERSION}-${INPUT_ID}.tar.bz2"
 
 DOCKER_IAMGES=(
     trentos_build_${RELEASE_VERSION}.bz2 \
     trentos_test_${RELEASE_VERSION}.bz2 \
 )
 
+ARCHIVE_SMALL=TRENTOS-M_SDK_no_docker_${RELEASE_VERSION}
+ARCHIVE_BIG=TRENTOS-M_SDK_${RELEASE_VERSION}
+
+
+#-------------------------------------------------------------------------------
+function export_image()
+{
+    local IMAGE_ID=${1}
+    local IMAGE_ARCHIVE=${2}
+
+    echo "saving ${IMAGE_ID} to ${IMAGE_ARCHIVE} ..."
+    docker inspect --format='{{.Config.Image}}' ${IMAGE_ID}
+    docker save ${IMAGE_ID} | pv | bzip2 > ${IMAGE_ARCHIVE}.bz2
+}
+
+
+#-------------------------------------------------------------------------------
+function pull_and_archive_image()
+{
+    local IMAGE_ID=${1}
+
+    echo "container: ${IMAGE_ID}"
+
+    # docker pull ${IMAGE_ID}
+    docker inspect --format='{{.Config.Image}}' ${IMAGE_ID}
+
+    # remove "docker:5000/" prefix
+    local STAND_ALONE_IMAGE_ID=${IMAGE_ID#*/}
+    echo "stand alone ID: ${STAND_ALONE_IMAGE_ID}"
+    docker tag ${IMAGE_ID} ${STAND_ALONE_IMAGE_ID}
+
+    # "trentos_build:trentos_0.9" -> "trentos_build-0.9"
+    IMAGE_ARCHIVE=${STAND_ALONE_IMAGE_ID%:*}_${STAND_ALONE_IMAGE_ID##*_}
+    export_image ${STAND_ALONE_IMAGE_ID} ${IMAGE_ARCHIVE}
+}
+
 #-------------------------------------------------------------------------------
 function do_tar()
 {
-    TAR_FILE=$1
-    TAR_FOLDER=$2
+    local TAR_FILE=$1
+    local TAR_FOLDER=$2
 
     # fix file order and timestamps
     tar -cjf ${TAR_FILE} \
@@ -38,36 +68,52 @@ function do_tar()
 #-------------------------------------------------------------------------------
 function do_untar()
 {
-    TAR_FILE=$1
-    TAR_FOLDER=$2
+    local TAR_FILE=$1
+    local TAR_FOLDER=$2
 
     mkdir -p ${TAR_FOLDER}
     tar -xf ${TAR_FILE} -C ${TAR_FOLDER}
 }
 
 #-------------------------------------------------------------------------------
+function do_repackage()
+{
+    local ARCHIVE_NAME=${1}
+
+    do_tar ${ARCHIVE_NAME}.bz2 pkg
+    do_untar ${ARCHIVE_NAME}.bz2 ${ARCHIVE_NAME}
+    diff -r pkg ${ARCHIVE_NAME}
+}
+
 #-------------------------------------------------------------------------------
-do_untar ${INPUT_PACKAGE} ${WORKSPACE_CREATE}/sdk
+#-------------------------------------------------------------------------------
 
-ln -s sdk/doc/pdf/TRENTOS-M_GettingStarted_SDK_V${RELEASE_VERSION}.pdf ${WORKSPACE_CREATE}/GettingStarted.pdf
-ls -l ${WORKSPACE_CREATE}
+#pull_and_archive_image docker:5000/trentos_test:trentos_${RELEASE_VERSION}
+#pull_and_archive_image docker:5000/trentos_build:trentos_${RELEASE_VERSION}
 
-do_tar ${TIMESTAMP}/${ARCHIVE_SMALL}.bz2 ${WORKSPACE_CREATE}
-do_untar ${TIMESTAMP}/${ARCHIVE_SMALL}.bz2 ${WORKSPACE_VERIFY}
-diff -r ${WORKSPACE_CREATE} ${WORKSPACE_VERIFY}
+#export_image trentos_test:trentos_${RELEASE_VERSION} trentos_test_${RELEASE_VERSION}
+#export_image trentos_build:trentos_${RELEASE_VERSION} trentos_build_${RELEASE_VERSION}
 
-rm -rf ${WORKSPACE_VERIFY}
 
-OUT_BASE_DOCKER=${WORKSPACE_CREATE}/docker
-mkdir -p ${OUT_BASE_DOCKER}
-sha256sum ${DOCKER_IAMGES[@]} >${OUT_BASE_DOCKER}/trentos_${RELEASE_VERSION}.sha256sums
-cp ${DOCKER_IAMGES[@]} ${OUT_BASE_DOCKER}
+mkdir ${TIMESTAMP}
+cd ${TIMESTAMP}
+do_untar ../${INPUT_PACKAGE} pkg/sdk
 
-do_tar ${TIMESTAMP}/${ARCHIVE_BIG}.bz2 ${WORKSPACE_CREATE}
-do_untar ${TIMESTAMP}/${ARCHIVE_BIG}.bz2 ${WORKSPACE_VERIFY}
-diff -r ${WORKSPACE_CREATE} ${WORKSPACE_VERIFY}
+ln -s sdk/doc/pdf/TRENTOS-M_GettingStarted_SDK_V${RELEASE_VERSION}.pdf pkg/GettingStarted.pdf
+ls -l pkg/sdk
 
+do_repackage ${ARCHIVE_SMALL}
+
+mkdir -p pkg/docker
 (
-    cd ${TIMESTAMP}
-    sha256sum ${ARCHIVE_SMALL}.bz2 ${ARCHIVE_BIG}.bz2 > trentos_SDK_v${RELEASE_VERSION}.sha256sums
+    cd pkg/docker
+    cp ${DOCKER_IAMGES[@]/#/../../../} .
+    sha256sum ${DOCKER_IAMGES[@]} >trentos_${RELEASE_VERSION}.sha256sums
 )
+
+do_repackage ${ARCHIVE_BIG}
+
+sha256sum \
+    ${ARCHIVE_SMALL}.bz2 \
+    ${ARCHIVE_BIG}.bz2 \
+    > trentos_SDK_v${RELEASE_VERSION}.sha256sums
