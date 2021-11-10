e8b8c58c1363a9f2d54417a73a5cf40d5928adc8
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.de>
2021-11-10 10:19:05 +0100

    [NEW] Add script to verify pdf hyperlinks
    Script will find all the hyperlinks in the pdf files in the current
    directory, and test if they are valid, and print out failing once.
    
    See:
    SEOS-3200

---
+++ verify_links.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -Eeuxo pipefail
+
+#-------------------------------------------------------------------------------
+# Copyright (C) 2021, HENSOLDT Cyber GmbH
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Finds all the hyperlinks in the pdf files in the current directory, and checks
+# if they are valid by issuing an http request, and prints out the failing ones.
+#
+# Example:
+#   cd <SDK_FOLDER>/sdk/doc/pdf
+#   <DEV_SCRIPTS_FOLDER>/dev-scripts/verify_links.sh
+#-------------------------------------------------------------------------------
+
+# Find all pdfs in the current dir (not recursively)...
+find . -maxdepth 1 -print0 -name "*.pdf" | \
+    # ...convert all pdfs to html...
+    xargs -0 -n 1 -I % pdftohtml -s -i -q -stdout % "found_pdf_file.html" | \
+    # ...find all hyperlinks...
+    grep -Po "<[a|img]+\\s+(?:[^>]*?\\s+)?[src|href]+=[\"']\K([^\"']*)(?=\")" |\
+    # ...filter out internal links...
+    grep -v "found_pdf_file.html" | \
+    # ...remove duplicates to optimize the number of http requests...
+    sort -u | \
+    # ...for each link check if valid by sending an http/s request and print
+    #    if link is invalid.
+    xargs -n 1 -I % \
+        sh -c 'curl --output /dev/null --silent --head --fail "%" || echo "%"'
